43cead46365c08b67e6fb07bc69178cd79f4b611
Kristen Halper <khalper@microsoft.com>
2022-12-13 16:14:20 -0800

    Add fold padding parameter

--- FoldAwareColumn/app/src/main/java/com/example/foldawarecolumn/ui/FoldAwareColumn.kt
+++ FoldAwareColumn/app/src/main/java/com/example/foldawarecolumn/ui/FoldAwareColumn.kt
@@ -1,5 +1,6 @@
 package com.example.foldawarecolumn.ui
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -8,6 +9,7 @@ import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.toComposeRect
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.platform.LocalDensity
 import androidx.window.layout.DisplayFeature
 import androidx.window.layout.FoldingFeature
 
@@ -23,17 +25,34 @@ import androidx.window.layout.FoldingFeature
 fun FoldAwareColumn(
     displayFeatures: List<DisplayFeature>,
     modifier: Modifier = Modifier,
+    foldPadding: PaddingValues = PaddingValues(),
     content: @Composable () -> Unit,
     // TODO: would be useful if we could add "fold padding" (parameter, arrangement, or modifier attribute?) and
     //  normal column capabilities, like an AdaptiveColumnScope that can be used to express weight
 ) {
-    // Extract fold properties if horizontal
+    // Extract folding feature if horizontal
     val fold = displayFeatures.find {
         it is FoldingFeature && it.orientation == FoldingFeature.Orientation.HORIZONTAL
     } as FoldingFeature?
-    val foldBoundsPx = fold?.bounds?.toComposeRect()
+
+    // Calculate fold bounds in pixels (including any added fold padding)
+    val foldBoundsPx = with(LocalDensity.current) {
+        val topPaddingPx = foldPadding.calculateTopPadding().roundToPx()
+        val bottomPaddingPx = foldPadding.calculateBottomPadding().roundToPx()
+
+        fold?.bounds?.toComposeRect()?.let {
+            Rect(
+                top = it.top - topPaddingPx,
+                left = it.left,
+                right = it.right,
+                bottom = it.bottom + bottomPaddingPx
+            )
+        }
+    }
+
+    // Extract other folding feature properties
     val foldHeightPx = foldBoundsPx?.height ?: 0
-    val foldIsOccluding = fold?.occlusionType == FoldingFeature.OcclusionType.FULL
+    val foldIsSeparating = fold?.isSeparating
 
     Layout(
         modifier = modifier.wrapContentSize(),
@@ -43,12 +62,9 @@ fun FoldAwareColumn(
             measurable.measure(constraints)
         }
 
-        val maxPlaceableWidth = placeables.fold(0) { acc, placeable ->
-            if (placeable.width > acc)
-                placeable.width
-            else acc
-        }
-
+        // Calculate max placeable width and height
+        val maxPlaceableWidth =
+            placeables.fold(0) { acc, placeable -> if (placeable.width > acc) placeable.width else acc }
         val totalPlaceableHeight = placeables.fold(0) { acc, placeable -> acc + placeable.height }
 
         // TODO: accounts for placeable height and fold height, but what about when there's extra empty space due
@@ -69,8 +85,8 @@ fun FoldAwareColumn(
                 )
                 val absoluteBounds = relativeBounds.translate(layoutBounds.left, layoutBounds.top)
 
-                // If fold is occluding and placeable overlaps fold, push placeable below fold
-                if (foldBoundsPx != null && foldIsOccluding && absoluteBounds.overlaps(foldBoundsPx)) {
+                // If fold is separating and placeable overlaps fold, push placeable below fold
+                if (foldIsSeparating == true && foldBoundsPx?.let { absoluteBounds.overlaps(it) } == true) {
                     placeableY = (foldBoundsPx.bottom - layoutBounds.top).toInt()
                 }
 
